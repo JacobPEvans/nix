6b542b6f901051d232f3f1f8a228f05c957ec976
JacobPEvans <20714140+JacobPEvans@users.noreply.github.com>
2025-11-08 06:24:26 -0500

    All changes copied from the repo. This isn't even a quarter of the files, so it will have lots of fun errors.

---
+++ themes/default.nix
@@ -0,0 +1,15 @@
+#
+#  Themes
+#
+#  flake.nix
+#   ├─ ./hosts
+#   │   └─ configuration.nix
+#   └─ ./modules
+#       └─ ./themes
+#           ├─ default.nix *
+#           └─ ...
+#
+
+[
+  ./themes.nix
+]
